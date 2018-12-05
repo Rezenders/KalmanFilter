e114609cb7846839f9aafe56a33e4828294aece2
rezenders <gustavorezendesilva@hotmail.com>
2018-12-05 01:04:42 -0200

    💩 EFK working. Code too slow ...

--- dataFusion.m
+++ dataFusion.m
@@ -6,16 +6,16 @@ Ts = 10^-2;
 t = 0:Iterations-1;
 t = t*Ts;
 
-gpsDeviation = 0.5;
-imuDeviation = 0.9;
+gpsDeviation = 0.2;
+imuDeviation = 1;
 
 radius = 15;
 l = 45;
 teta = pi/2;
-vel_max = 20*pi;
+vel_max = 2*pi;
 phi = 0:(vel_max/Iterations):vel_max;
 
-R = rotationMatrix(teta);
+Rot = rotationMatrix(teta);
 A = [.5 .5 0; 0 0 1; 1/(2*l) -1/(2*l) 0];
 
 
@@ -36,15 +36,20 @@ PosIMU = zeros(2, Iterations);
 time_passed = 0;
 last_gps = 0;
 
-P = zeros(5,5); %Covariance
+Q = eye(3)*0.3;
+R = eye(3)*0.8;
+
+Pk = ones(5,5); %Covariance
 Xe = zeros(5, Iterations);
 Ptrace = zeros(1,Iterations);
+
+Z = zeros(2,Iterations);
 
 for i=2:Iterations
   %%Modelo do robo
   U = [radius*phi(i-1) radius*phi(i-1) 0]';
   VelR(:, i) = A*U;
-  VelI(:, i) = inv(R)*VelR(:,i);
+  VelI(:, i) = inv(Rot)*VelR(:,i);
 
   AcelR = (VelR(:,i) - VelR(:,i-1))/Ts;
   AcelI = (VelI(:,i) - VelI(:,i-1))/Ts;
@@ -54,27 +59,28 @@ for i=2:Iterations
 
   time_passed = time_passed + Ts;
   if((time_passed - last_gps)>(Ts*100))
-      ZGps(:,i) = [PosI(1, i-1) PosI(2, i-1) sqrt(VelI(1, i-1)^2 + VelI(2, i-1)^2)]' + normrnd(0, gpsDeviation);
-      % last_gps = time_passed;
+      ZGps(:,i) = [PosI(1, i) PosI(2, i) sqrt(VelI(1, i)^2 + VelI(2, i)^2)]' + normrnd(0, gpsDeviation);
   else
       ZGps(:,i) = ZGps(:,i-1);
-  endif
+  end
+  
 
   %INS
-  IMU(:,i-1) = [AcelR(1) AcelR(2) VelR(3, i-1)]' + normrnd(0, imuDeviation);
+  IMU(:,i) = [AcelR(1) AcelR(2) VelR(3, i)]' + normrnd(0, imuDeviation);
   TetaIMU(1, i) = TetaIMU(1, i-1) + IMU(3,i-1)*Ts;
-  RIMU = rotationMatrix(TetaIMU(1, i-1));
-  IMUI(:,i-1) = inv(RIMU)*IMU(:,i-1);
+  RIMU = rotationMatrix(TetaIMU(1, i));
+  IMUI(:,i) = inv(RIMU)*IMU(:,i);
 
   VelIMU(:, i) = VelIMU(:, i-1) + IMUI(1:2, i-1)*Ts;
   PosIMU(:, i) = PosIMU(:, i-1) + VelIMU(:, i-1)*Ts;
 
-  ZIns(:,i) = [PosIMU(1, i-1) PosIMU(2, i-1) VelIMU(1, i-1) VelIMU(2, i-1)]';
+  ZIns(:,i) = [PosIMU(1, i) PosIMU(2, i) VelIMU(1, i) VelIMU(2, i)]';
 
   %EFK
   if((time_passed - last_gps)>(Ts*100))
     Xe(:,i) = Xe(:, i-1);
-    Ptrace(i) = trace(P);
+    Ptrace(i) = trace(Pk);
+    last_gps = time_passed;
   else
     F = [0 0 0 1 0; 0 0 0 0 1; 0 0 0 0 0; 0 0 -IMUI(2, i-1) 0 0; 0 0 IMUI(1, i-1) 0 0]; %Conferir sinais e ver se usa i ou i-1
     G = [0 0 0; 0 0 0; 1 0 0; 0 cos(TetaIMU(1,i-1)) -sin(TetaIMU(1,i-1)); 0 sin(TetaIMU(1,i-1)) cos(TetaIMU(1,i-1))];
@@ -84,13 +90,31 @@ for i=2:Iterations
         H = [1 0 0 0 0; 0 1 0 0 0; 0 0 0 0 0];
     else
         H = [1 0 0 0 0; 0 1 0 0 0; 0 0 0 ZIns(3,i-1)/v_sqrt ZIns(4,i-1)/v_sqrt];
-    endif
+    end
+    
     D = zeros(3,3);
 
-    [Fd, Gd, Hd, Dd] = c2d(F,G,H,D,Ts*100);
-  endif
-endfor
+    [Fd, Gd, Hd, Vd] = c2dm(F,G,H,D,Ts*100);
+    
+    dZ = [ZGps(1,i)-ZIns(1,i), ZGps(2,i)-ZIns(2,i), ZGps(3,i)-sqrt(ZIns(3,i)^2 +ZIns(4,i)^2)]';
+    
+    %Pred
+    Xk =0;
+    Pk = Fd*Pk*Fd' + Gd*Q*Gd';
+    
+    %Filter update
+    K = Pk * H' *inv(H*Pk*H' + R');
+    Xe(:,i) = K*dZ;
+    Pk = (eye(5) - K * H) * Pk;
+    Ptrace(i) = trace(Pk);
+       
+%     Z(:,i) = [(ZIns(1,i) + Xe(1,i)), (ZIns(2,i)+Xe(2,i))]';
+%     ZIns(1:2,i) + Xe(1:2,i);
+  end
+  Z(1,i) = ZIns(1,i) + Xe(1,i);
+  Z(2,i) = ZIns(2,i) + Xe(2,i);
+end
 
-plot(t, ZIns(2,:), 'k');
-hold
-plot(t, ZGps(2,:), 'b');
+figure(1)
+plot(t, ZIns(2,:), 'r', t, ZGps(2,:), 'b',t, Z(2,:), 'k', t, PosI(2,:),'y');
+% plot(t, ZGps(2,:));
