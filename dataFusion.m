50b71f6aa98af73e38d697f43b6b038b9a103e00
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-12-04 22:41:12 -0200

    ✨ F,G,H

--- dataFusion.m
+++ dataFusion.m
@@ -36,6 +36,10 @@ PosIMU = zeros(2, Iterations);
 time_passed = 0;
 last_gps = 0;
 
+P = zeros(5,5); %Covariance
+Xe = zeros(5, Iterations);
+Ptrace = zeros(1,Iterations);
+
 for i=2:Iterations
   %%Modelo do robo
   U = [radius*phi(i-1) radius*phi(i-1) 0]';
@@ -51,7 +55,7 @@ for i=2:Iterations
   time_passed = time_passed + Ts;
   if((time_passed - last_gps)>(Ts*100))
       ZGps(:,i) = [PosI(1, i-1) PosI(2, i-1) sqrt(VelI(1, i-1)^2 + VelI(2, i-1)^2)]' + normrnd(0, gpsDeviation);
-      last_gps = time_passed;
+      % last_gps = time_passed;
   else
       ZGps(:,i) = ZGps(:,i-1);
   endif
@@ -67,6 +71,24 @@ for i=2:Iterations
 
   ZIns(:,i) = [PosIMU(1, i-1) PosIMU(2, i-1) VelIMU(1, i-1) VelIMU(2, i-1)]';
 
+  %EFK
+  if((time_passed - last_gps)>(Ts*100))
+    Xe(:,i) = Xe(:, i-1);
+    Ptrace(i) = trace(P);
+  else
+    F = [0 0 0 1 0; 0 0 0 0 1; 0 0 0 0 0; 0 0 -IMUI(2, i-1) 0 0; 0 0 IMUI(1, i-1) 0 0]; %Conferir sinais e ver se usa i ou i-1
+    G = [0 0 0; 0 0 0; 1 0 0; 0 cos(TetaIMU(1,i-1)) -sin(TetaIMU(1,i-1)); 0 sin(TetaIMU(1,i-1)) cos(TetaIMU(1,i-1))];
+
+    v_sqrt = sqrt(ZIns(3,i-1)^2 + ZIns(4,i-1)^2); %ZIns?
+    if(v_sqrt==0)
+        H = [1 0 0 0 0; 0 1 0 0 0; 0 0 0 0 0];
+    else
+        H = [1 0 0 0 0; 0 1 0 0 0; 0 0 0 ZIns(3,i-1)/v_sqrt ZIns(4,i-1)/v_sqrt];
+    endif
+    D = zeros(3,3);
+
+    [Fd, Gd, Hd, Dd] = c2d(F,G,H,D,Ts*100);
+  endif
 endfor
 
 plot(t, ZIns(2,:), 'k');
